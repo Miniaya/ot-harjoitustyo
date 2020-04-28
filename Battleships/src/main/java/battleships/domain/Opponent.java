0ca14be1f73836730685a88c3b9443bfeeea4c07
Miniaya <henna.makiluoma@gmail.com>
2020-04-28 23:01:30 +0300

    Tietokonevastustajan alustava versio

--- Battleships/src/main/java/battleships/domain/Opponent.java
+++ Battleships/src/main/java/battleships/domain/Opponent.java
@@ -1,17 +1,32 @@
 
 package battleships.domain;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Random;
-
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.robot.Robot;
 
 public class Opponent {
     
     ArrayList<Integer>[] shots;
     Random r;
+    GridPane gameboard;
+    boolean hit;
+    int[] prev;
+    ShipService service;
     
+    public Opponent(GridPane gameboard) throws SQLException {
+        shots = new ArrayList[11];
+        r = new Random();
+        this.gameboard = gameboard;
+        service = new ShipService();
+        
+        for (int i = 1; i <= 10; i++) {
+            shots[i] = new ArrayList<>();
+        }
+    }
     
     public Opponent() {
         shots = new ArrayList[11];
@@ -20,6 +35,10 @@ public class Opponent {
         for (int i = 1; i <= 10; i++) {
             shots[i] = new ArrayList<>();
         }
+    }
+    
+    public void setGameboard(GridPane board) {
+        this.gameboard = board;
     }
     
     public int[] shoot() {
@@ -38,8 +57,32 @@ public class Opponent {
         
         shots[x].add(y);
         
+//        if(isHit(x, y)){
+//            hit = true;
+//            int[] prev = {x, y};
+//            this.prev = prev;
+//        }
+
         int[] shot = {x, y};
-        
         return shot;
+    }
+    
+    public boolean isHit(int x, int y) throws SQLException {
+        
+        for (Node node : gameboard.getChildren()) {
+            if (GridPane.getColumnIndex(node) == x + 1 && GridPane.getRowIndex(node) == y + 1) {
+                Button btn = (Button) node;
+                
+                if(service.isShip(x, y, 2) > 0){
+                    btn.setText("O");
+                    return true;
+                } else {
+                    btn.setText("X");
+                    return false;
+                }
+            }
+        }
+        
+        return false;
     }
 }
